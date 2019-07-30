dfbeb4e51f4e7d2462eacc49fd09b2f5ec67e51b
Lili Du <lizumi-chan@hotmail.com>
2019-07-30 11:59:52 +0200

    Refactor VizCircle and Skills

--- src/components/vizCircle/Skills.js
+++ src/components/vizCircle/Skills.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as d3 from 'd3';
+import { calculateLinks } from './actions';
 
 class Skills extends React.Component {
   constructor(props) {
@@ -9,41 +9,12 @@ class Skills extends React.Component {
     };
   }
 
-  componentDidMount(props) {
-    this.calculateLinks();
+  componentDidMount() {
+    this.setState({ nodes: calculateLinks(this.props) });
   }
 
   componentWillReceiveProps(props) {
-    this.calculateLinks();
-  }
-
-  calculateLinks() {
-    //Create a hierarchy and sort it alphabetically
-    const data = this.props.skills;
-    const root = d3
-      .hierarchy(data)
-      .sort((a, b) => a.data.name.localeCompare(b.data.name));
-    //create a tree layout an process the
-    const treeLayout = d3.tree();
-
-    //angle scale to calculate the angle they should be based on their X distance
-    const angleScale = d3
-      .scaleLinear()
-      .domain([0, 1])
-      .range([0, 360]);
-
-    const layOut = treeLayout(root);
-
-    for (let i in layOut.children) {
-      let node = layOut.children[i];
-      node.angle = angleScale(node.x); //set angle in radians
-      node.textRotation = -node.angle;
-      node.anchorText = node.angle < 90 || node.angle > 270 ? 'start' : 'end';
-    }
-
-    this.setState({
-      nodes: layOut
-    });
+    this.setState({ nodes: calculateLinks(props) });
   }
 
   render() {
@@ -80,8 +51,8 @@ class Skills extends React.Component {
             );
           })
         ) : (
-            <g />
-          )}
+          <g />
+        )}
       </g>
     );
   }
