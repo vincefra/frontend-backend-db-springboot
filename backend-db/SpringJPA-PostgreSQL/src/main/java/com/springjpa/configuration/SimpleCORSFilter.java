7f0c82920d2b28313abe9cd2999e51ca350aabc9
Vincent <vincent.frank@yh.nackademin.se>
2019-11-19 16:15:01 +0100

    added cors

--- backend-db/SpringJPA-PostgreSQL/src/main/java/com/springjpa/configuration/SimpleCORSFilter.java
+++ backend-db/SpringJPA-PostgreSQL/src/main/java/com/springjpa/configuration/SimpleCORSFilter.java
@@ -17,7 +17,7 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type");
         response.setHeader("Access-Control-Expose-Headers", "Location");
         filterChain.doFilter(servletRequest, servletResponse);
     }
