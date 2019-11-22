ce8088eaef73a805b4bfcf039be1c6436c9cfc38
Vincent <vincent.frank@yh.nackademin.se>
2019-11-22 11:03:22 +0100

    combined frontend into backend, sharing same Java springboot tomcat

---
+++ backend-db/SpringJPA-PostgreSQL/src/main/resources/public/precache-manifest.95f34450cfb22fba5a8717faf803dbcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b600d5e545ee5830bf6e82e798ccf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b5c984b1e8dca4fb946",
+    "url": "/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "fbdeff59da3caceb7b8f",
+    "url": "/static/css/main.d2b2f4d2.chunk.css"
+  },
+  {
+    "revision": "9b5c984b1e8dca4fb946",
+    "url": "/static/js/2.d8c9051a.chunk.js"
+  },
+  {
+    "revision": "fbdeff59da3caceb7b8f",
+    "url": "/static/js/main.83eb4987.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
