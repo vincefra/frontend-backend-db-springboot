0e91efa912756f5ed1dc9357543dc4b17e164cbb
Vincent <vincent.frank@yh.nackademin.se>
2019-11-27 10:22:15 +0100

    updated build

---
+++ backend-db/SpringJPA-PostgreSQL/src/main/resources/static/precache-manifest.1653c9c64b3f660f4170d6192ef5e2ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47404510ebbaa7b113ac8dc05b223201",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b5c984b1e8dca4fb946",
+    "url": "/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "e3b1315d8343b110f1f6",
+    "url": "/static/css/main.d2b2f4d2.chunk.css"
+  },
+  {
+    "revision": "9b5c984b1e8dca4fb946",
+    "url": "/static/js/2.d8c9051a.chunk.js"
+  },
+  {
+    "revision": "e3b1315d8343b110f1f6",
+    "url": "/static/js/main.a89bb49c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
