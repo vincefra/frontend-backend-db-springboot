4404eaae71646369154ac0c4a562fdb9f89a5f18
Vincent <vincent.frank@yh.nackademin.se>
2019-11-28 14:24:26 +0100

    Merge origin/refactoring into dev-dbf
    Conflicts:
    	src/server/employee/api.js
    	src/server/main/api.js

--- backend-db/SpringJPA-PostgreSQL/src/main/java/com/springjpa/configuration/SimpleCORSFilter.java
+++ backend-db/SpringJPA-PostgreSQL/src/main/java/com/springjpa/configuration/SimpleCORSFilter.java
@@ -16,7 +16,8 @@ public class SimpleCORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "*"
+                + "");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type");
